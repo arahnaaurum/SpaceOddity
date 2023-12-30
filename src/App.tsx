bbd9d4c6c959c20b5e71c3d8d0468d295a8ddc80
arahnaaurum <arahna.aurum@gmail.com>
2023-12-31 01:17:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import './App.css';
+import { QuestionList } from './components/questionList';
+import { Person, QUESTIONS } from './constant';
+import { Enter } from './components/enter';
+
+function App() {
+  const [page, setPage] = useState<number>(0);
+  const [score, setScore] = useState<number[]>([]);
+  const [currentAnswer, setCurrentAnswer] = useState<number>(0);
+  const [folks, setFolks] = useState<boolean>(false);
+  const handleClick = () => {
+    window.scroll(0,0);
+    setPage(page + 1);
+    setScore((prev) => ([...prev, currentAnswer]));
+  }
+  
+  const calculateScore = () => {
+      Person.forEach((person) => person.score = score.filter((value) => (value === person.id)).length)
+      Person.sort((a, b) => b.score-a.score);
+      const answer = Person[0];
+      return (<>
+          <h2>Итак, вы... {answer.name}</h2>
+          <p className='violet'>{answer.text}</p>
+          <div>
+            <img src={answer.img} alt='answer' width='100%'/>
+          </div>
+        </>
+      )
+  }
+
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Space Oddity</h1>
+      </header>
+      <main className='main'>
+        <div className='container'>
+          {page === 0 && !folks &&
+            <Enter
+              page={page}
+              setPage={setPage}
+            />
+          }
+          {page > 0 && page <= QUESTIONS.length && !folks &&
+            <>
+              <QuestionList
+                page={page}
+                setCurrentAnswer={setCurrentAnswer}
+              />
+              <button className='button' onClick={handleClick}>
+                  Дальше
+              </button>
+            </>
+          }
+          {page > QUESTIONS.length && !folks &&
+            <>
+              {calculateScore()}
+              <button className='button' onClick={() => setFolks(true)}>Спасибо за игру!</button>
+            </>
+          }
+          {folks && <>
+            <iframe
+                  width="550"
+                  height="350"
+                  src={`https://www.youtube.com/embed/iYYRH4apXDo`}
+                  allowFullScreen
+                  title="Embedded youtube"
+                />
+          </>}
+        </div>
+      </main>
+      <footer className='footer'>
+        (c) <a href='https://acomics.ru/~spaceoddity'>Space Oddity</a>  by natlalihuitl
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/questionList.tsx
@@ -0,0 +1,45 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import { QUESTIONS } from "../constant"
+
+export const QuestionList = ({
+  page,
+  setCurrentAnswer
+}: {
+  page: number;
+  setCurrentAnswer: Dispatch<SetStateAction<number>>
+}) => {
+  const { id, question, answers } = QUESTIONS[page - 1];
+
+  return (
+    <div className="">
+      <p className="violet">{id}. {question}</p>
+      {answers.map(({ person, text, mem }) => (
+        <>
+          <label key={person} className="question">
+            <input
+              name="person"
+              type="radio"
+              value={person}
+              onChange={(e) => setCurrentAnswer(+e.target.value)}
+            />
+            {person?
+              <p>{text}</p> :
+              <div className="video-responsive">
+                <iframe
+                  width="420"
+                  height="240"
+                  src={`https://www.youtube.com/embed/${text}`}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  title="Embedded youtube"
+                />
+              </div>
+            }
+          </label>
+          {mem && <div className="mem"> <img src={mem} width='500' alt='mem' /></div>}
+        </>
+      ))}
+
+    </div>
+  )
+}
